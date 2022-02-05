018aaaad6625e93109613d53cee539c7b0c2d47f
olayemi <greaterthings266@gmail.com>
2022-02-05 16:52:43 +0000

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh streamlit_setup.sh && streamlit run BMI_Calculator.py
---
+++ streamlit_setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit
+
+echo "[server]
+
+headless = true
+
+port = $PORT
+
+enableCORS = false
+
+" > ~/.streamlit/config.toml
